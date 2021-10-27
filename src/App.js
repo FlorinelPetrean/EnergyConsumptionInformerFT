f0b30688f8066e4ca14f71f0a49bd1992804625e
florin.petrean <florin.petrean98@gmail.com>
2021-10-27 13:19:55 +0300

    added deploy files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NavigationBar from './navigation-bar'
+import Home from './home/home';
+import PersonContainer from './person/person-container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ErrorPage from './commons/errorhandling/error-page';
+import styles from './commons/styles/project-style.css';
+
+class App extends React.Component {
+
+
+    render() {
+
+        return (
+            <div className={styles.back}>
+            <Router>
+                <div>
+                    <NavigationBar />
+                    <Switch>
+
+                        <Route
+                            exact
+                            path='/'
+                            render={() => <Home/>}
+                        />
+
+                        <Route
+                            exact
+                            path='/person'
+                            render={() => <PersonContainer/>}
+                        />
+
+                        {/*Error*/}
+                        <Route
+                            exact
+                            path='/error'
+                            render={() => <ErrorPage/>}
+                        />
+
+                        <Route render={() =><ErrorPage/>} />
+                    </Switch>
+                </div>
+            </Router>
+            </div>
+        )
+    };
 }
 
-export default App;
+export default App
