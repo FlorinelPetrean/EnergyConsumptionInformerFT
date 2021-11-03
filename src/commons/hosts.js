eb4f8a47082cd424d8f4fc7dab03515e8b9096c6
florin.petrean <florin.petrean98@gmail.com>
2021-11-03 14:03:09 +0200

    test deploy

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,4 +1,4 @@
 export const HOST = {
-    backend_api: 'http://localhost:8080',
-    //backend_api: 'https://ds2021energyutlityplatform.herokuapp.com',
+    //backend_api: 'http://localhost:8080',
+    backend_api: 'https://ds2021energyutlityplatform.herokuapp.com',
 };
